ba5293203427a4534f24d6425a29d4356aa5f989
Lloyd-Heartfire <beaudonloic@hotmail.com>
2026-01-06 18:23:13 +0100

    For the component 'Timeline'

---
+++ src/data/experiences.js
@@ -0,0 +1,53 @@
+export const experiences = [
+    {
+        title: "Stagiaire Développeur Fullstack & Machine Learning",
+        company: "Pôle Formation UIMM Reims",
+        period: "Juin - Septembre 2025",
+        location: "Reims, FRANCE",
+        description: "Développement avec PHP Laravel et React, implémentation de Machine Learning pour recommandations automatiques",
+        type: "experience"
+    },
+    {
+        title: "Stagiaire Concepteur Designer UI/UX",
+        company: "LouiseM",
+        period: "Mai - Septembre 2024",
+        location: "Saint-Cloud, FRANCE",
+        description: "Conception de maquette graphique, catalogue de décoration 2024, collaboration avec BIC, montage vidéo",
+        type: "experience"
+    },
+    {
+        title: "Stagiaire Référent Digital",
+        company: "Taxi Ferauge",
+        period: "Août - Septembre 2023",
+        location: "Villers-Semeuse, FRANCE",
+        description: "Gestion de projet, maquettage avec Figma et création de site Internet avec WordPress",
+        type: "experience"
+    }
+];
+
+export const formations = [
+    {
+        title: "Développeur IA / Data Science",
+        school: "Pôle Formation UIMM",
+        period: "Avril - Décembre 2025",
+        location: "Charleville-Mézières",
+        status: "Titre reçu",
+        type: "formation"
+    },
+    {
+        title: "Concepteur Deisgner UI/UX",
+        school: "Pôle Formation UIMM",
+        period: "Janvier - Octobre 2024",
+        location: "Charleville-Mézières",
+        status: "Titre reçu",
+        type: "formation"
+    },
+    {
+        title: "Référent Digital",
+        school: "Pôle Formation UIMM",
+        period: "Avril - Novembre 2023",
+        location: "Charleville-Mézières",
+        status: "Titre reçu",
+        type: "formation"
+    },
+]
