bc334f0bea50b752d98df134328e11476cf55ac4
Lloyd-Heartfire <beaudonloic@hotmail.com>
2026-01-06 20:36:03 +0100

    Project and Skills

---
+++ src/data/skills.js
@@ -0,0 +1,15 @@
+export const devSkills = [
+    "React", "TypeScript", "JavaScript", "Python", "Django", "Node.js", "PHP", "PHP Laravel", "PostgreSQL", "Tailwind CSS", "HTML", "CSS"
+];
+
+export const mlSkills = [
+    "PyTorch", "Scikit-learn", "Pandas", "Numpy", "Chart.js", "D3.js", "Matplotlib", "Power BI"
+];
+
+export const designSkills = [
+    "Figma", "Affinity Designer", "Affinity Photo", "Affinity Publisher", "Clip Studio Paint", "DaVinci Resolve", "Canva", "UI/UX Design", "FigJam"
+];
+
+export const toolsSkills = [
+    "Git", "Docker", "VSCode", "Kubernetes", "Trello", "Wordpress", "Shopify", "Postman"
+];
